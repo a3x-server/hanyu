9ac758459ecc1a63a4f1b3547836f5b26809a8a8
serverBKD <carlosm1bravoa@gmail.com>
2025-07-22 09:12:36 -0400

    2025JUL22: v0.1.11: CSS Table + Hover + BTN color

--- src/components/table-hanyu.jsx
+++ src/components/table-hanyu.jsx
@@ -16,7 +16,7 @@ export default function TableHanyu() {
 				console.error( '获取汉语数据失败' )
 				return
 			}
-			console.log( '获取汉语数据成功', HANYU )
+			// console.log( '获取汉语数据成功', HANYU )
 			setHANYU( HANYU )
 		}
 		callHanyu()
@@ -24,46 +24,43 @@ export default function TableHanyu() {
 	}, [] )
 
 	return (
-		<section className="hanyu-table-container">
+		<section className="hanyu-table-section">
 			<article className="hanyu-table-article">
-				<div className="hanyu-table-div">
-					<table className="table">
-						<thead className="table-header">
-							<tr className="header-row">
-								<th className="cell first-col">汉字</th>
-								<th className="cell">拼音</th>
-								<th className="cell tone-col">音调吗</th>
-								<th className="cell">西班呀语</th>
-								<th className="cell end-col">删除</th>
+				<table className="table">
+					<thead className="table-header">
+						<tr className="header-row">
+							<th className="cell first-col">汉字</th>
+							<th className="cell">拼音</th>
+							{/* <th className="cell tone-col">音调吗</th> */ }
+							<th className="cell">西班呀语</th>
+							<th className="cell end-col">删除</th>
+						</tr>
+					</thead>
+					<tbody className="table-body">
+						{ HANYU?.map( ( elm ) => (
+							<tr key={ elm.id }>
+								<td className="cell hanzi">{ elm.hanzi }</td>
+								<td className="cell">{ elm.pinyin }</td>
+								{/* <td className="cell tone-col">{ elm.tone }</td> */ }
+								<td className="cell">{ elm.xinbanya }</td>
+								<td className="cell delete-col">
+									<ButtonEraser target={ elm.id } text="删除" />
+								</td>
 							</tr>
-						</thead>
-						<tbody className="table-body">
-							{ HANYU?.map( ( elm ) => (
-								<tr key={ elm.id }>
-									<td className="cell hanzi">{ elm.hanzi }</td>
-									<td className="cell">{ elm.pinyin }</td>
-									<td className="cell tone-col">{ elm.tone }</td>
-									<td className="cell">{ elm.xinbanya }</td>
-									<td className="cell delete-col">
-										<ButtonEraser target={ elm.id } text="删除" />
-									</td>
-								</tr>
-							) ) }
-						</tbody>
-						<tfoot className="table-footer">
-							<tr>
-								<th>1</th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-							</tr>
-						</tfoot>
-					</table>
-
-				</div>
+						) ) }
+					</tbody>
+					<tfoot className="table-footer">
+						<tr>
+							<th>1</th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+						</tr>
+					</tfoot>
+				</table>
 			</article>
 		</section>
 	)
